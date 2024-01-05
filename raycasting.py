f89a913a5851e667661f54ef6fc512a518d9dd25
Andressa Varela <andressaavarela@gmail.com>
2024-01-05 17:25:43 -0300

    Initial setup

---
+++ raycasting.py
@@ -0,0 +1,25 @@
+import pygame as pg
+import math
+from settings import *
+
+
+class RayCasting:
+    def __init__(self, game):
+        self.game = game
+
+    def ray_cast(self):
+        ox,  oy = self.game.player.pos
+        x_map, y_map = self.game.player.map_pos
+
+        ray_angle = self.game.player.angle - HALF_FOV + 0.0001
+        for ray in range(NUM_RAYS):
+            sin_a = math.sin(ray_angle)
+            cos_a = math.cos(ray_angle)
+
+            # verticals
+            x_vert, dx = (x_map + 1, 1) if cos_a > 0 else (x_map - 1e-6, -1)
+
+            ray_angle += DELTA_ANGLE
+
+    def update(self):
+        self.ray_cast()
